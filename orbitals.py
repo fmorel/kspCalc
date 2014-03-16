be50925b273a199baa80ed9509e24ff349f32a31
François-Xavier MOREL <efix.morel@gmail.com>
2014-03-16 21:33:06 +0100

    It seems to work :)

--- orbitals.py
+++ orbitals.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
-
+import math
 import csv
 import argparse
 import sys
 import re
 
 nameHeader = 'Celestial Body (Reference code)'
-refHeader = 'Parent Body Reference Code'
+parentHeader = 'Parent Body Reference Code'
+smaHeader = 'Semimajor_axis'
+periodHeader = 'Sidereal_period'
 
 #Helper functions
 def fetchPlanet(planetNameOrCode):
 	for planet in planets:
-		if planet[nameHeader].find(planetNameOrCode) >= 0:
+		if planet[nameHeader].find(str(planetNameOrCode)) >= 0:
 			return planet
 	return None
 
@@ -19,7 +21,18 @@ def getCode(planet):
 	m = re.search("\((?P<code>[0-9]+)\)",planet[nameHeader])
 	return int(m.group('code'))
 
+def getOrbitalParameter(planet):
+	sma = planet[smaHeader]
+	period = planet[periodHeader]
+	return (period*period) / (sma*sma*sma)
 
+def changePlanetParams(planet):
+	planet[parentHeader] = int(planet[parentHeader])
+	planet[smaHeader] = float(planet[smaHeader].replace(',',''))/1000
+	planet[periodHeader] = float(planet[periodHeader].replace(',',''))
+	return planet
+
+		
 
 #Open csv data retrieved from http://wiki.kerbalspaceprogram.com/wiki/Celestials
 csvFile = open('orbitals.csv', 'r')
@@ -30,37 +43,49 @@ planets = []
 for row in csvDict :
 	planets.append(row)
 csvFile.close()
+del planets[0]
 
-print planets[2]
 #Parse arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("start", help="Starting celestial body")
 parser.add_argument("dest", help="Destination body")
-parser.add_argument("-a", "--altitude", help="Current radius of spacecraft orbit")
+parser.add_argument("-a", "--altitude", help="Current radius of spacecraft orbit in kilometers")
 args = parser.parse_args()
 
 startPlanet = fetchPlanet(args.start)
 destPlanet = fetchPlanet(args.dest)
 
+#Change parant field so it is an integer
+planets = map (changePlanetParams, planets)
+planetToSatellite = False
+
+#Detect error cases
 if startPlanet is None  or destPlanet is None:
 	print "Planets not found"
-	sys.exit();
+	sys.exit()
 
-if int(destPlanet[refHeader]) == getCode(startPlanet) and args.altitude is None:
-	print "Destination body orbits around Starting body, I need altitude of current orbit"
-	sys.exit();
+if int(destPlanet[parentHeader]) != getCode(startPlanet) and destPlanet[parentHeader] != startPlanet[parentHeader]:
+	print "Incompatible orbiting characteristics of bodies"
+	sys.exit()
+
+if destPlanet[parentHeader] == getCode(startPlanet) :
+	planetToSatellite = True
+	if args.altitude is None :
+		print "Destination body orbits around Starting body, I need radius of current orbit"
+		sys.exit()
+	else : 
+		radius = args.altitude
+else :
+	if args.altitude is None :
+		radius = startPlanet[smaHeader]
+	else :
+		radius =args.altitude
+
+radius = float(radius)	
+transferSma = 0.5*(radius + destPlanet[smaHeader])
+transferTime = 0.5*math.sqrt(math.pow(transferSma,3) * getOrbitalParameter(destPlanet))
+
+print "Destination planet will travel ", 360 * (transferTime / destPlanet[periodHeader]), "degrees while transfer orbit"
 
 
-
-#headers = csvData[0]
-#print "Header length is : ",len(headers)
-#for row in csvData[2:] :
-#	planet={}
-#	print "Row length is : ",len(row)
-#	for j,cell in enumerate(row) :
-#		if j < len(headers) and headers[j] != '':
-#			planet[headers[j]] = cell
-#	planets.append(planet)
-
-#print planets[0]
 
